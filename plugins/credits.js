36adc79f00d6662e5bdd0e9e46731a05199b4040
mrsanon <106508721+mrsanon@users.noreply.github.com>
2022-06-16 09:10:35 -0700

    Update credits.js

--- plugins/credits.js
+++ plugins/credits.js
@@ -2,8 +2,8 @@ let handler = async (m, { usedPrefix}) => {
 let credit = `
 \t\t\t\t\t\t\t*THANKS TO*
 
-*TOXIC ALIEN:*
-*https://github.com/Alien-alfa*
+*MRSANON:*
+*https://github.com/mrsanon*
 
 *D-3-V-1-L:*
 *https://github.com/D-3-V-1-L*
@@ -16,7 +16,7 @@ let credit = `
 
 *AND MYSELF*
 `
- conn.sendTBI(m.chat, credit, wm, 'https://i.pinimg.com/474x/de/bd/b2/debdb24645169bf95eecd49f3144315f.jpg', `SOURCE CODE`, `https://github.com/Neeraj-x0/Millie-MD`, null, null, 'Menu', `${usedPrefix}menu`, null, null, null, null, m) 
+ conn.sendTBI(m.chat, credit, wm, 'https://i.pinimg.com/474x/de/bd/b2/debdb24645169bf95eecd49f3144315f.jpg', `SOURCE CODE`, `https://github.com/mrsanon/slasia`, null, null, 'Menu', `${usedPrefix}menu`, null, null, null, null, m) 
 }
 handler.help = ['credits']
 handler.tags = ['info']
